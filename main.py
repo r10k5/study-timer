e58baf5914cba88f4fe09f7b6ec5206c3b4046e4
r10k5 <r10k5.job@gmail.com>
2024-11-26 21:11:59 +0300

    отрефакторена кнопка запуска таймера

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 import flet as ft
-
+        
 def main(page: ft.Page):
     page.title = "HomeTimer"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-    global button_status
     button_status = "Start"
 
-    def change_button_style(e):
-        global button_status
-
-        if button_status == "Start":
+#не меняет цвет потому что разрабы не фиксят этот баг, не трогать - ждать обнову
+    def change_main_button_style(e):
+        if main_button.data:
             main_button.bgcolor = ft.colors.GREEN_200
-            button_status = "Pause"
+            button_status = "Stop"
         else:
             main_button.bgcolor = ft.colors.RED_200
             button_status = "Start"
-
+        
+        main_button.data = not main_button.data
         main_button.text = button_status
         page.update()
 
-    main_button = ft.ElevatedButton(text=button_status, on_click=change_button_style)
+    main_button = ft.ElevatedButton(text=button_status, 
+                                    data=True, 
+                                    bgcolor = ft.colors.GREEN_200, 
+                                    width = 300,
+                                    height = 300,
+                                    on_click=change_main_button_style)
     main_button.width = 300
     main_button.height = 300
 
@@ -35,7 +39,5 @@ def main(page: ft.Page):
             alignment=ft.MainAxisAlignment.CENTER,
         ),
     )
-
-    
 
 ft.app(main)
