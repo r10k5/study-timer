3369385042194ca2d72233ebefe7451cdb72ef48
r10k5 <r10k5.job@gmail.com>
2024-11-26 22:06:00 +0300

    добавлен таймер, но он ломает изменение кнопки

--- main.py
+++ main.py
@@ -1,21 +1,42 @@
 import flet as ft
-        
+import time
+import asyncio
+       
+async def update_timer(page, timer_text, start_time):
+    while True:
+        left_time = time.time() - start_time
+        timer_text.value = f"{left_time:.2f} seconds"
+        page.update()
+        await asyncio.sleep(0.1)
+
 def main(page: ft.Page):
     page.title = "HomeTimer"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
     button_status = "Start"
+    timer_value = ft.Text(value="0.00 sec")
+
+    async def start_timer():
+        start_time = time.time()
+        await update_timer(page, timer_value, start_time)
+        
 
 #не меняет цвет потому что разрабы не фиксят этот баг, не трогать - ждать обнову
-    def change_main_button_style(e):
+    async def change_main_button_style(e):
+        nonlocal button_status
+        #хз что это
         if main_button.data:
-            main_button.bgcolor = ft.colors.GREEN_200
-            button_status = "Stop"
-        else:
             main_button.bgcolor = ft.colors.RED_200
+            button_status = "Stop"
+            main_button.data = False
+            await start_timer()
+        else:
+            main_button.bgcolor = ft.colors.GREEN_200
             button_status = "Start"
+            main_button.data = True
         
-        main_button.data = not main_button.data
+        main_button.data = True
+        # main_button.data = not main_button.data
         main_button.text = button_status
         page.update()
 
@@ -25,10 +46,9 @@ def main(page: ft.Page):
                                     width = 300,
                                     height = 300,
                                     on_click=change_main_button_style)
-    main_button.width = 300
-    main_button.height = 300
 
     page.add(
+        timer_value,
         ft.Row(
             [
                 ft.Container(
@@ -39,5 +59,6 @@ def main(page: ft.Page):
             alignment=ft.MainAxisAlignment.CENTER,
         ),
     )
+    
 
 ft.app(main)
