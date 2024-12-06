aa60c8228827e5269dea552fa3170338bc945fd3
r10k5 <r10k5.job@gmail.com>
2024-12-06 05:44:11 +0300

    добавлено: секундомер при паузе сохраняет пройденное время

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ import flet as ft
 import time
 import asyncio
        
-async def update_timer(page, timer_text, start_time):
-    while True:
-        left_time = time.time() - start_time
+timer_is_run = False
+left_time = 0.0
+
+async def update_timer(page, timer_text):
+    global timer_is_run
+    global left_time
+    start_time = time.time()
+
+    while timer_is_run:
+        left_time += time.time() - start_time
+        start_time = time.time()
+
         timer_text.value = f"{left_time:.2f} seconds"
         page.update()
         await asyncio.sleep(0.1)
@@ -12,37 +21,34 @@ async def update_timer(page, timer_text, start_time):
 def main(page: ft.Page):
     page.title = "HomeTimer"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
-
-    button_status = "Start"
     timer_value = ft.Text(value="0.00 sec")
 
     async def start_timer():
-        start_time = time.time()
-        await update_timer(page, timer_value, start_time)
+        await update_timer(page, timer_value)
         
 
     async def change_main_button_style(e):
-        nonlocal button_status
-        print(button_status)
+        global timer_is_run
 
-        if main_button.data:
+        if not timer_is_run:
+            timer_is_run = True
             main_button.bgcolor = ft.Colors.RED_200
             main_button.text = "Stop"
-            main_button.data = False
             await start_timer()
         else:
+            timer_is_run = False
             main_button.bgcolor = ft.Colors.GREEN_200
             main_button.text = "Start"
-            main_button.data = True
         
         page.update()
 
-    main_button = ft.ElevatedButton(text = button_status, 
-                                    data = True, 
-                                    bgcolor = ft.colors.GREEN_200, 
-                                    width = 300,
-                                    height = 300,
-                                    on_click=change_main_button_style)
+    main_button = ft.ElevatedButton(
+        text = "Start", 
+        bgcolor = ft.colors.GREEN_200, 
+        width = 300,
+        height = 300,
+        on_click=change_main_button_style
+    )
 
     page.add(
         timer_value,
